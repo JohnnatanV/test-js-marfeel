2b354319481044ceae26970e1f52f4105f06666a
Johnnatan <joh.villaneda@outlook.com>
2021-10-12 19:59:28 -0500

    Reset the html, css and js files to use another method

--- js/app.js
+++ js/app.js
@@ -1,65 +1,15 @@
-class GraphScreen extends HTMLElement {
-  constructor() {
-    super();
+const boxChart = document.querySelectorAll(".box__chart");
 
-    let shadow = this.attachShadow({ mode: "open" });
-    this.graphBox = document.createElement("div");
-    this.value = document.createElement("div");
+const data = require("../data.json");
 
-    shadow.appendChild(this.graphBox);
-    this.graphBox.appendChild(this.value);
-  }
+console.log(data);
 
-  connectedCallback() {
-    let graphName = this.getAttribute("graph");
-    let url = this.getAttribute("data");
+function getData() {
+  let names = document.querySelectorAll("[name]");
 
-    function numberWithDots(x) {
-      return x
-        .toString()
-        .replace(/^0+/, "")
-        .replace(/\D/g, "")
-        .replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-    }
-
-    this.value.innerHTML = "";
-
-    fetch(url)
-      .then((response) => response.json())
-      .then((charts) =>
-        charts.forEach((chart) => {
-          if (chart.graph === graphName) {
-            this.value.innerHTML = `
-              <style>
-                * {
-                font-family: 'Open sans', sans-serif; 
-              }
-      
-              .graph__tittle {
-                text-transform: uppercase;
-                font-size: 1.1em;
-                color: #aaa;
-                margin: 0;
-              }
-              .value {
-                font-size: 1.5em;
-              }
-              </style>
-                <div class="donutChart">
-                  <h2 class="graph__tittle">
-                    ${graphName}  
-                  </h2>
-                  <div class="value">
-                  ${numberWithDots(
-                    parseInt(chart["tablet"]) + parseInt(chart["smartphone"])
-                  )}
-                  </div>
-                </div>
-              `;
-          }
-        })
-      );
-  }
+  names.forEach((name) => {
+    console.log(name);
+  });
 }
 
-window.customElements.define("graph-screen", GraphScreen);
+getData();
